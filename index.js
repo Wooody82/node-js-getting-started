38ac2b0049dcbcde0e6d09b4ffdd6069d0c76494
Wooody82 <malasaad82@gmail.com>
2023-06-13 10:39:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const path = require('path')
+const axios = require('axios');
 
 const PORT = process.env.PORT || 5001
 
 console.log('Hello!!!');
+makeRequestWithoutPool();
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -11,3 +13,15 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+
+const makeRequestWithoutPool = async () => {
+    const start = process.hrtime();
+    const response = await axios.get('https://api.kucoin.com/api/v1/market/allTickers');
+    const timestamp = response.data.data;
+    const end = process.hrtime(start);
+    const durationInMs = (end[0] * 1000 + end[1] / 1000000).toFixed(2);
+    console.log(`API without Pool Took ${durationInMs} milliseconds.`);
+    // console.log('Timestamp:', timestamp);
+};
